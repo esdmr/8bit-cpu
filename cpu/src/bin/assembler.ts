c5ab81e51baf61e2e7dc30d49703c585564a4adb
esdmr <esdmr0@gmail.com>
2020-08-22 23:36:53 +0430

    Modify imports

--- cpu/src/bin/assembler.ts
+++ cpu/src/bin/assembler.ts
@@ -1,22 +1,22 @@
-import { inspect } from 'util';
-import { argv } from 'process';
-import { readFileSync, writeFileSync } from 'fs';
+import * as util from 'util';
+import * as process from 'process';
+import * as fs from 'fs';
 import { CPU } from '../cpu.js';
-import { Assembler, BankData } from '../asm.js';
+import { Assembler } from '../asm.js';
 
 function main () {
-	if (![4, 5].includes(argv.length)) {
+	if (![4, 5].includes(process.argv.length)) {
 		console.log('Output file not provided.');
-		console.log(`Usage: node ${argv[1]} INPUT --eval [--debug]`);
-		console.log(`Usage: node ${argv[1]} INPUT OUTPUT`);
+		console.log(`Usage: node ${process.argv[1]} INPUT --eval [--debug]`);
+		console.log(`Usage: node ${process.argv[1]} INPUT OUTPUT`);
 		process.exit(1);
 	}
 
-	const inputFile = argv[2];
-	const outputFile = argv[3];
-	const shouldDebug = argv[4] === '--debug';
-	const input = readFileSync(inputFile, 'utf8');
+	const inputFile = process.argv[2];
+	const outputFile = process.argv[3];
+	const shouldDebug = process.argv[4] === '--debug';
 
+	const input = fs.readFileSync(inputFile, 'utf8');
 	const asm = Assembler.assemble(input);
 
 	if (outputFile === '--eval') {
@@ -33,7 +33,7 @@ function main () {
 		cpu.start();
 
 		if (shouldDebug) {
-			console.log(inspect(cpu, {
+			console.log(util.inspect(cpu, {
 				colors: true,
 				depth: Infinity,
 				maxArrayLength: Infinity,
