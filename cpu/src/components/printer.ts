8f81eff1b66c3a6161856e8bd5971f02cdda4e68
esdmr <esdmr0@gmail.com>
2020-09-16 17:43:01 +0430

    Keep Printer DRY

--- cpu/src/components/printer.ts
+++ cpu/src/components/printer.ts
@@ -1,31 +1,28 @@
 import { Component, RW } from './base.js';
 import Controller from './controller.js';
 import { CPU } from '../cpu.js';
+import { Memory } from './index.js';
 
 export default class Printer extends Component {
-	readonly buffer = new Uint8Array(128);
+	readonly memory: Memory;
 
 	constructor (readonly opts: { readonly bank: number, readonly addr: number; }) {
 		super();
+		this.memory = new Memory({ bank: opts.bank });
 	}
 
 	onAttached (cpu: CPU) {
+		cpu.bus.on(this.memory);
 		const component = cpu.bus.find(Controller);
 		if (component == null) throw new Error('No controller availiable');
 
 		component.addControl(this.opts.addr, () => {
-			console.log(String.fromCharCode(...this.buffer).replace(/\0$/g, ''));
-			return 0;
+			console.log(String.fromCharCode(...this.memory.buffer).replace(/\0$/g, ''));
+			return undefined;
 		});
 	}
 
-	handler ({ bp, rw, addr, data }: Component.Options) {
-		if (bp !== this.opts.bank) return;
-
-		if (rw === RW.WRITE) {
-			this.buffer[addr] = data;
-		} else if (rw === RW.READ) {
-			return this.buffer[addr];
-		}
+	handler () {
+		return undefined;
 	}
 }
